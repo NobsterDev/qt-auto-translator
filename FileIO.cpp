acb5c7280fa176224a43252e21a42ed9c7376131
NobsterDev <furkanka10@gmail.com>
2026-01-07 16:47:30 +0300

    style

--- FileIO.cpp
+++ FileIO.cpp
@@ -7,7 +7,7 @@
 
 FileIO::FileIO()
 {
-	m_tsWordList = new std::map<QString, QStringList**>();
+	m_tsWordList = new std::map<QString, QStringList **>();
 	m_currentTranslationMap = new QMap<QString, QString>();
 	entrySeperator = "\t";
 }
@@ -18,36 +18,36 @@ FileIO::~FileIO()
 	delete m_currentTranslationMap;
 }
 
-FileIOError FileIO::generateFile( int fileType, QString outputFileName )
+FileIOError FileIO::generateFile(int fileType, QString outputFileName)
 {
 	QFile outputFile(outputFileName);
 
-	if ( !outputFile.open( QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text ) )
+	if (!outputFile.open(QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text))
 	{
 		qDebug() << "Cannot create the output file. Please re-check." << endl;
 		return FileNotFound;
 	}
 
-	if (fileType == TRANSLATABLE_FILE || fileType == ONLINE_FILE )
+	if (fileType == TRANSLATABLE_FILE || fileType == ONLINE_FILE)
 	{
 		QTextStream outputStream(&outputFile);
 
-		QList<std::map<QString, QStringList**>*> wordList;
+		QList<std::map<QString, QStringList **> *> wordList;
 		QStringList sectionName;
-		
+
 		wordList.append(m_tsWordList);
 		sectionName.append(TS_SECTION);
 
 		if (fileType == TRANSLATABLE_FILE)
 		{
-			for (int i = 0; i < m_tranlatableWordList.size(); i++ )
+			for (int i = 0; i < m_tranlatableWordList.size(); i++)
 			{
 				outputStream << m_tranlatableWordList.at(i) << entrySeperator << m_tranlatedWordList.at(i) << endl;
 			}
 		}
 		else if (fileType == ONLINE_FILE)
 		{
-			for (int i = 0; i < m_tranlatableWordList.size(); i++ )
+			for (int i = 0; i < m_tranlatableWordList.size(); i++)
 			{
 				outputStream << m_tranlatableWordList.at(i) << endl;
 			}
@@ -66,28 +66,28 @@ FileIOError FileIO::generateFile( int fileType, QString outputFileName )
 	}
 }
 
-FileIOError FileIO::generateOnlineTranslatableFile( QString outputFileName /*= ONLINE_FILE_NAME*/ )
+FileIOError FileIO::generateOnlineTranslatableFile(QString outputFileName /*= ONLINE_FILE_NAME*/)
 {
 	return generateFile(ONLINE_FILE, outputFileName);
 }
 
-FileIOError FileIO::generateMannuallyTranslatableFile( QString outputFileName /*= OUTPUT_FILE*/ )
+FileIOError FileIO::generateMannuallyTranslatableFile(QString outputFileName /*= OUTPUT_FILE*/)
 {
 	return generateFile(TRANSLATABLE_FILE, outputFileName);
 }
 
-FileIOError FileIO::extractTranslatesFromTsFile( QString tsFileName)
+FileIOError FileIO::extractTranslatesFromTsFile(QString tsFileName)
 {
 	QDomDocument wordsXmlTree;
 	QFile xmlFile(tsFileName);
 
-	if ( !xmlFile.open(QIODevice::ReadOnly) )
+	if (!xmlFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found." << endl;
 		return FileNotFound;
 	}
 
-	if ( !wordsXmlTree.setContent(&xmlFile) )
+	if (!wordsXmlTree.setContent(&xmlFile))
 	{
 		qDebug() << "Invalid content in the XML file : Reading TS File. " << tsFileName << endl;
 		xmlFile.close();
@@ -105,7 +105,7 @@ FileIOError FileIO::extractTranslatesFromTsFile( QString tsFileName)
 	m_tranlatableWordList.append(TS_SECTION);
 	m_tranlatedWordList.append("#");
 
-	for (int i = 0; i< contextsNodes.size(); i++)
+	for (int i = 0; i < contextsNodes.size(); i++)
 	{
 		QDomElement context = contextsNodes.item(i).toElement();
 
@@ -117,7 +117,7 @@ FileIOError FileIO::extractTranslatesFromTsFile( QString tsFileName)
 			continue;
 		}
 
-		QStringList** words;
+		QStringList **words;
 
 		if (m_tsWordList->find(contextName.text()) != m_tsWordList->end())
 		{
@@ -125,15 +125,15 @@ FileIOError FileIO::extractTranslatesFromTsFile( QString tsFileName)
 		}
 		else
 		{
-			words = new QStringList*[2];
+			words = new QStringList *[2];
 			words[0] = new QStringList();
 			words[1] = new QStringList();
-			m_tsWordList->insert(std::pair<QString, QStringList**>(contextName.text(), words));
+			m_tsWordList->insert(std::pair<QString, QStringList **>(contextName.text(), words));
 		}
 
 		QDomNodeList messageNodes = context.elementsByTagName("message");
 
-		for (int j = 0; j< messageNodes.size(); j++)
+		for (int j = 0; j < messageNodes.size(); j++)
 		{
 			QDomNode message = messageNodes.at(j);
 
@@ -158,14 +158,14 @@ FileIOError FileIO::extractTranslatesFromTsFile( QString tsFileName)
 			words[0]->append(sourceString);
 			words[1]->append(translatedString);
 
-			if ( !m_tranlatableWordList.contains(sourceString))
+			if (!m_tranlatableWordList.contains(sourceString))
 			{
 				m_tranlatableWordList.append(sourceString);
 				m_tranlatedWordList.append(translatedString);
 				m_currentTranslationMap->insert(sourceString, translatedString);
 
 				m_onlineTranslableWordList.append(sourceString);
-				//qDebug() << source.at(0).toElement().text() << "\t" << translated.at(0).toElement().text() << endl;
+				// qDebug() << source.at(0).toElement().text() << "\t" << translated.at(0).toElement().text() << endl;
 			}
 		}
 
@@ -180,19 +180,18 @@ FileIOError FileIO::extractTranslatesFromTsFile( QString tsFileName)
 	return ReadSuccess;
 }
 
-FileIOError FileIO::extractLanguageFromTsFile( QString tsFileName, QString& langCode )
+FileIOError FileIO::extractLanguageFromTsFile(QString tsFileName, QString &langCode)
 {
 	QDomDocument tsFileXml;
 	QFile xmlFile(tsFileName);
 
-
-	if ( !xmlFile.open(QIODevice::ReadOnly) )
+	if (!xmlFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found." << endl;
 		return FileNotFound;
 	}
 
-	if ( !tsFileXml.setContent(&xmlFile) )
+	if (!tsFileXml.setContent(&xmlFile))
 	{
 		qDebug() << "Invalid content in the XML file : Reading TS File. " << tsFileName << endl;
 		xmlFile.close();
@@ -200,7 +199,7 @@ FileIOError FileIO::extractLanguageFromTsFile( QString tsFileName, QString& lang
 	}
 
 	QDomNodeList tsNodes = tsFileXml.elementsByTagName("TS");
-	
+
 	if (tsNodes.size() != 1)
 		return InvalidFile;
 
@@ -209,7 +208,7 @@ FileIOError FileIO::extractLanguageFromTsFile( QString tsFileName, QString& lang
 	return ReadSuccess;
 }
 
-FileIOError FileIO::translateWithTranslatedFile( QString translatableFileName /*= ONLINE_FILE_NAME*/, QString translatedFileName /*= TRANSLATED_FILE_NAME*/ )
+FileIOError FileIO::translateWithTranslatedFile(QString translatableFileName /*= ONLINE_FILE_NAME*/, QString translatedFileName /*= TRANSLATED_FILE_NAME*/)
 {
 	m_tranlatedWordList.clear();
 	m_currentTranslationMap->clear();
@@ -218,7 +217,7 @@ FileIOError FileIO::translateWithTranslatedFile( QString translatableFileName /*
 	QFile translatableFile(translatableFileName);
 
 	// Load the translated phrases
-	if ( !translatableFile.open(QIODevice::ReadOnly) )
+	if (!translatableFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found : " << translatableFileName << " . Please  re-check." << endl;
 		translatableFile.close();
@@ -227,14 +226,14 @@ FileIOError FileIO::translateWithTranslatedFile( QString translatableFileName /*
 
 	QTextStream translatableFileStream(&translatableFile);
 
-	while(!translatableFileStream.atEnd())
+	while (!translatableFileStream.atEnd())
 	{
-		QString line = translatableFileStream.readLine().trimmed();    
+		QString line = translatableFileStream.readLine().trimmed();
 
 		m_tranlatableWordList.append(line);
 	}
 
-	if ( !translatedFile.open(QIODevice::ReadOnly) )
+	if (!translatedFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found : " << translatedFileName << " . Please  re-check." << endl;
 		translatedFile.close();
@@ -243,23 +242,23 @@ FileIOError FileIO::translateWithTranslatedFile( QString translatableFileName /*
 
 	QTextStream translatedFileStream(&translatedFile);
 
-	while(!translatedFileStream.atEnd())
+	while (!translatedFileStream.atEnd())
 	{
-		QString line = translatedFileStream.readLine().trimmed();    
-		
+		QString line = translatedFileStream.readLine().trimmed();
+
 		m_tranlatedWordList.append(line);
 	}
 
 	// ----
 
-	if ( m_tranlatableWordList.size() != m_tranlatedWordList.size() )
+	if (m_tranlatableWordList.size() != m_tranlatedWordList.size())
 	{
 		qDebug() << "Invalid translate file : " << translatedFileName << " . Entry count doesn't match with the extracted entries." << endl;
 		qDebug() << m_tranlatableWordList.size() << " " << m_tranlatedWordList.size() << endl;
 		return InvalidFile;
 	}
 
-	for ( int i = 0; i < m_tranlatableWordList.size(); i++)
+	for (int i = 0; i < m_tranlatableWordList.size(); i++)
 	{
 		m_currentTranslationMap->insert(m_tranlatableWordList.at(i), m_tranlatedWordList.at(i));
 	}
@@ -268,11 +267,11 @@ FileIOError FileIO::translateWithTranslatedFile( QString translatableFileName /*
 	return ReadSuccess;
 }
 
-FileIOError FileIO::loadMannuallyTranslatedEntries( QString mannuallyTranslatedFileName )
+FileIOError FileIO::loadMannuallyTranslatedEntries(QString mannuallyTranslatedFileName)
 {
 	QFile mannuallyTranslatedFile(mannuallyTranslatedFileName);
 
-	if ( !mannuallyTranslatedFile.open(QIODevice::ReadOnly) )
+	if (!mannuallyTranslatedFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found : " << mannuallyTranslatedFileName << " . Please  re-check. " << mannuallyTranslatedFile.errorString() << endl;
 		return FileNotFound;
@@ -280,16 +279,16 @@ FileIOError FileIO::loadMannuallyTranslatedEntries( QString mannuallyTranslatedF
 
 	QTextStream mannualFileStream(&mannuallyTranslatedFile);
 
-	while(!mannualFileStream.atEnd())
+	while (!mannualFileStream.atEnd())
 	{
-		QString line = mannualFileStream.readLine(); 
-		
+		QString line = mannualFileStream.readLine();
+
 		if (line.trimmed().isEmpty())
 			continue;
 
 		QStringList fields = line.split(entrySeperator);
 
-		if ( !line.startsWith(lineCommentString) && fields.size() != 2)
+		if (!line.startsWith(lineCommentString) && fields.size() != 2)
 		{
 			qDebug() << "Invalid file format : " << mannuallyTranslatedFileName << " at '" << line << "' . Please  re-check." << endl;
 			return InvalidFile;
@@ -303,27 +302,26 @@ FileIOError FileIO::loadMannuallyTranslatedEntries( QString mannuallyTranslatedF
 			m_currentTranslationMap->insert(sourceText, translatedText);
 
 			// Remove if the translation is provided by the user
-			if ( m_onlineTranslableWordList.contains(sourceText) )
+			if (m_onlineTranslableWordList.contains(sourceText))
 				m_onlineTranslableWordList.removeOne(sourceText);
-
 		}
 	}
 	mannuallyTranslatedFile.close();
 	return ReadSuccess;
 }
 
-FileIOError FileIO::applyTranslatesToTsFile( QString tsFileName )
+FileIOError FileIO::applyTranslatesToTsFile(QString tsFileName)
 {
 	QDomDocument wordsXmlTree;
 	QFile xmlFile(tsFileName);
 
-	if ( !xmlFile.open(QIODevice::ReadOnly) )
+	if (!xmlFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found." << endl;
 		return FileNotFound;
 	}
 
-	if ( !wordsXmlTree.setContent(&xmlFile) )
+	if (!wordsXmlTree.setContent(&xmlFile))
 	{
 		qDebug() << "Invalid content in the XML file." << endl;
 		return InvalidXmlFile;
@@ -335,13 +333,13 @@ FileIOError FileIO::applyTranslatesToTsFile( QString tsFileName )
 	{
 		QDomNode message = messageNodes.at(i);
 		QString word = message.toElement().elementsByTagName("source").at(0).toElement().text();
-		
-		if ( m_currentTranslationMap->find(word) != m_currentTranslationMap->end() )
+
+		if (m_currentTranslationMap->find(word) != m_currentTranslationMap->end())
 		{
 			QString translatedString = m_currentTranslationMap->find(word).value();
 			QDomNode translation = message.toElement().elementsByTagName("translation").at(0);
 
-			QDomElement newTranslationTag = wordsXmlTree.createElement(QString("translation")); 
+			QDomElement newTranslationTag = wordsXmlTree.createElement(QString("translation"));
 			QDomText newNodeText = wordsXmlTree.createTextNode(translatedString);
 			newTranslationTag.appendChild(newNodeText);
 
@@ -350,13 +348,13 @@ FileIOError FileIO::applyTranslatesToTsFile( QString tsFileName )
 				qDebug() << word << " " << translation.toElement().text() << " " << translatedString << endl;
 			}
 			message.toElement().replaceChild(newTranslationTag, translation);
-			//translation.toElement().setNodeValue(translatedString);
+			// translation.toElement().setNodeValue(translatedString);
 		}
 	}
 
 	xmlFile.close();
 
-	if ( !xmlFile.open(QFile::ReadWrite | QIODevice::Truncate | QIODevice::Text) )
+	if (!xmlFile.open(QFile::ReadWrite | QIODevice::Truncate | QIODevice::Text))
 	{
 		qDebug() << "File not found." << endl;
 		return FileNotFound;
@@ -372,19 +370,19 @@ FileIOError FileIO::applyTranslatesToTsFile( QString tsFileName )
 	return WriteSuccess;
 }
 
-FileIOError FileIO::changeLanguageOfTsFile( QString tsFileName, QString langCode )
+FileIOError FileIO::changeLanguageOfTsFile(QString tsFileName, QString langCode)
 {
 	QDomDocument tsFileXml;
 	QFile xmlFile(tsFileName);
 
 	// Read the TS file
-	if ( !xmlFile.open(QIODevice::ReadOnly) )
+	if (!xmlFile.open(QIODevice::ReadOnly))
 	{
 		qDebug() << "File not found." << endl;
 		return FileNotFound;
 	}
 
-	if ( !tsFileXml.setContent(&xmlFile) )
+	if (!tsFileXml.setContent(&xmlFile))
 	{
 		qDebug() << "Invalid content in the XML file : Reading TS File. " << tsFileName << endl;
 		xmlFile.close();
@@ -406,12 +404,12 @@ FileIOError FileIO::changeLanguageOfTsFile( QString tsFileName, QString langCode
 	xmlFile.close(); // close the file, cz now the content is in the memory.
 
 	// Re-open the file to overwrite.
-	if ( !xmlFile.open(QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text) )
+	if (!xmlFile.open(QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text))
 	{
 		qDebug() << "File not found." << endl;
 		return FileNotFound;
 	}
-	
+
 	QTextStream stream(&xmlFile);
 	stream.setCodec("UTF-8"); // change the file codec to UTF-8.
 	tsFileXml.save(stream, 4);
@@ -420,11 +418,11 @@ FileIOError FileIO::changeLanguageOfTsFile( QString tsFileName, QString langCode
 	return ReadSuccess;
 }
 
-void FileIO::getOnlineTranslatableStrings( QStringList& translatableStrings, bool ignoreAlreadyTranslatedStrings /*= true*/ )
+void FileIO::getOnlineTranslatableStrings(QStringList &translatableStrings, bool ignoreAlreadyTranslatedStrings /*= true*/)
 {
 	translatableStrings.clear();
 
-	foreach( QString word, m_onlineTranslableWordList )
+	foreach (QString word, m_onlineTranslableWordList)
 		translatableStrings.append(word);
 
 	translatableStrings.removeDuplicates();
